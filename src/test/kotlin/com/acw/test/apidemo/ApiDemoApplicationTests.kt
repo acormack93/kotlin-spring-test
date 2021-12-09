b5163eb0dc9da655f15dd63ac2c17429c70bf596
Alex Cormack-Widdop <alexc@predictx.com>
2021-12-09 23:35:52 +0000

    first commit

---
+++ src/test/kotlin/com/acw/test/apidemo/ApiDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.acw.test.apidemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
